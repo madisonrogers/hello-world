2512e6d2bc4b6bf5c829d9b82018793dfe233c7b
Maddie Rogers <madisonrogers@mymail.mines.edu>
2017-02-15 10:34:03 -0700

    Modified the .gitignore file and added the HelloWorld file

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Hello world!");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld hw = new HelloWorld();
+	}
+
+}
